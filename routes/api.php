2ad814bef592a5d31fc01f0088df55138b7fdb37
DANU DWIKI LAKSANA <ddlgamers14@gmail.com>
2024-05-08 18:20:41 +0700

    auth jwt

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\CekToken;
+use Illuminate\Support\Facades\Route;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+Route::apiResource('user', UserController::class)->middleware(CekToken::class);
